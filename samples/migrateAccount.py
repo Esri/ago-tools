b9640cd0d88a6b94de252442f0f760df4086b53d
Evan Caldwell <evan.caldwell@gmail.com>
2014-02-13 20:28:43 -0500

    removed wrongful inclusion of class parameter
    Accidentally included passing the entire class object as an argument to `self`.

--- samples/migrateAccount.py
+++ samples/migrateAccount.py
@@ -9,7 +9,7 @@ from agoTools.admin import Admin
 myAgol = Admin('<username>')  # Replace <username> your ADMIN account
 
 # for migrating a single account...
-myAgol.migrateAccount(myAgol, '<userFrom>', '<userTo>')   # Replace with usernames between which you are moving items
+myAgol.migrateAccount('<userFrom>', '<userTo>')   # Replace with usernames between which you are moving items
 
 # for migrating a batch of accounts...
-myAgol.migrateAccounts(myAgol, <path to user mapping CSV>)   # Replace with path to CSV file with col1=userFrom, col2=userTo
+myAgol.migrateAccounts(<path to user mapping CSV>)   # Replace with path to CSV file with col1=userFrom, col2=userTo
