d8c02bd180f42105b9c8be6f5dcee7261568485f
Sam Berg <sam_berg@esri.com>
2014-03-13 13:24:48 -0400

    updateServiceItemsThumbnail.py
    For a portal or specific user folder, look for any blank thumbnails and
    replace with a default version.

---
+++ samples/updateServiceItemsThumbnail.py
@@ -0,0 +1,53 @@
+import csv
+import argparse
+import sys
+
+from agoTools.admin import Admin
+
+
+def _raw_input(prompt=None, stream=None, input=None):
+    # A raw_input() replacement that doesn't save the string in the
+    # GNU readline history.
+    if not stream:
+        stream = sys.stderr
+    if not input:
+        input = sys.stdin
+    prompt = str(prompt)
+    if prompt:
+        stream.write(prompt)
+        stream.flush()
+    # NOTE: The Python C API calls flockfile() (and unlock) during readline.
+    line = input.readline()
+    if not line:
+        raise EOFError
+    if line[-1] == '\n':
+        line = line[:-1]
+    return line
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--user')
+parser.add_argument('-p', '--password')
+parser.add_argument('-portal', '--portal')
+parser.add_argument('-folder', '--folder')
+
+args = parser.parse_args()
+
+if args.user == None:
+  args.user = _raw_input("Username:")
+
+if args.portal == None:
+  args.portal = _raw_input("Portal: ")
+ 
+if args.folder == None:
+  args.folder = _raw_input("Folder (optional): ")
+
+args.portal = str(args.portal).replace("http://","https://")
+
+agoAdmin = Admin(args.user,args.portal,args.password)
+
+if args.folder!= None:
+  fid = agoAdmin.getFolderID(args.folder)
+  args.folder=fid
+  folderid = '/' + args.folder
+
+agoAdmin.updateServiceItemsThumbnail(folderid)
