2d834529d5076d4f163ee66ac1407e8471a12234
Jeremy Mirmelstein <jmirmel@gmail.com>
2014-03-17 14:52:02 -0400

    Update createUserListCSV.py
    Encoded the Username, Full Name, and Email to account for users with accents in their names.

--- samples/createUserListCSV.py
+++ samples/createUserListCSV.py
@@ -13,4 +13,4 @@ with open(outputFile, 'wb') as output:
     dataWriter.writerow(['Full Name', 'Email', 'Username', 'Role', 'Date Created'])
     # Write user data.
     for user in users:
-        dataWriter.writerow([user['fullName'], user['email'], user['username'], user['role'], time.strftime("%Y-%m-%d",time.gmtime(user['created']/1000))])
+        dataWriter.writerow([user['fullName'].encode('utf-8'), user['email'].encode('utf-8'), user['username'].encode('utf-8'), user['role'], time.strftime("%Y-%m-%d",time.gmtime(user['created']/1000))])
