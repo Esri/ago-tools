3766a12d840bfcabce04b8de26eaa3c40ef8e412
Sam Berg <sam_berg@esri.com>
2014-03-24 15:44:23 -0400

    added clearGroup.py and admin.py/clearGroup
    Unshare (remove) all items from specified group.

---
+++ samples/clearGroup.py
@@ -0,0 +1,49 @@
+import argparse
+import sys
+
+from agoTools.admin import Admin
+
+def _raw_input(prompt=None, stream=None, input=None):
+    # A raw_input() replacement that doesn't save the string in the
+    # GNU readline history.
+    if not stream:
+        stream = sys.stderr
+    if not input:
+        input = sys.stdin
+    prompt = str(prompt)
+    if prompt:
+        stream.write(prompt)
+        stream.flush()
+    # NOTE: The Python C API calls flockfile() (and unlock) during readline.
+    line = input.readline()
+    if not line:
+        raise EOFError
+    if line[-1] == '\n':
+        line = line[:-1]
+    return line
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--user')
+parser.add_argument('-p', '--password')
+parser.add_argument('-portal', '--portal')
+parser.add_argument('-group', '--group')
+
+args = parser.parse_args()
+
+if args.user == None:
+  args.user = _raw_input("Username:")
+
+if args.portal == None:
+  args.portal = _raw_input("Portal: ")
+ 
+if args.group == None:
+  args.group = _raw_input("Group ID: ")
+
+args.portal = str(args.portal).replace("http://","https://")
+
+agoAdmin = Admin(args.user,args.portal,args.password)
+
+agoAdmin.clearGroup(args.group)
+
+
+
