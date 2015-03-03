776806f5d6d215e936e2388062b497c1fd947934
Sam Berg <sam_berg@esri.com>
2015-03-03 13:30:10 -0500

    remove test bat files

--- samples/populateBookmarks.bat
+++
@@ -1,1 +0,0 @@
-populateBookmarks.py -layerURL http://bosdemo2.esri.com/arcgis/rest/services/RevolutionaryWar/RevolutionaryWarBattlesP1/MapServer/0 -labelfield POINAME  -portal http://esrinortheast.maps.arcgis.com
